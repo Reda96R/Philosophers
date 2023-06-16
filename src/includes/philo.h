4aec684b21ccb51c68e4582d2fdfe97e23bb63ad
Reda96R <redarayyad@yahoo.com>
2023-06-16 15:52:00 +0100

    adding t_data struct

--- src/includes/philo.h
+++ src/includes/philo.h
@@ -3,6 +3,18 @@
 
 #include <stdio.h>
 
+typedef struct s_data
+{
+  int philos;
+  long long t_to_eat;
+  long long t_to_sleep;
+  long long t_to_die;
+  int       meals;
+  int       end_meal;
+} t_data;
 
+/*::: utils :::*/
+void ft_args_checker(int ac, char *av[]);
+void  ft_errors_buster(int err_id);
 
 #endif 
--- src/philo.c
+++ src/philo.c
@@ -2,5 +2,10 @@
 
 int main(int ac, char *av[])
 {
+  t_data data;
+
   ft_args_checker(ac, av);
+  data  = (t_data *)malloc(sizeof (t_data *));
+  if (!data)
+    return (1);
 }
